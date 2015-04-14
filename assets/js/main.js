02e3fcdfe27807d5f0625d0cb6f9d9b80f277ae6
Yevgeniy Brikman <brikis98@gmail.com>
2015-04-13 19:34:25 -0700

    Add photos and more fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-(function() {
+(function(window, $) {
   "use strict";
 
   $(".js-toggle").on('click', function(event) {
@@ -8,4 +8,28 @@
     var toggleId = target.data('toggle-id');
     $('#' + toggleId).toggle();
   });
-})();
+
+  var truncate = function(str, maxLength) {
+    return str.length > maxLength ? str.substring(0, maxLength) + "..." : str;
+  }
+
+  var showAlbums = function(albums) {
+    var albumData = _.map(albums.feed.entry, function(album) {
+      var title = truncate(album.title.$t, 20);
+      var thumbnail = album.media$group.media$thumbnail[0].url;
+      var url = _.find(album.link, function(link) { return link.rel === 'alternate'; }).href;
+
+      return {title: title, thumbnail: thumbnail, url: url};
+    });
+
+    var template = _.template($('#albums-template').html());
+    var albumHtml = template({albums: albumData});
+    $('#albums').html(albumHtml);
+  };
+
+  if (window.albums) {
+    showAlbums(window.albums);
+  } else {
+    window.storeAlbums = showAlbums;
+  }
+})(window, $);
