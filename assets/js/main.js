998fe95224623815d44761ab2dc7bd3770ee3a3e
Yevgeniy Brikman <brikis98@gmail.com>
2015-04-16 11:19:50 -0700

    Use G+ for photos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,5 @@
-(function(window, $) {
+(function() {
   "use strict";
 
   responsiveNav('.nav-collapse');
-
-  var truncate = function(str, maxLength) {
-    return str.length > maxLength ? str.substring(0, maxLength) + "..." : str;
-  };
-
-  var showAlbums = function(albums) {
-    var albumData = _.map(albums.feed.entry, function(album) {
-      var title = truncate(album.title.$t, 20);
-      var thumbnail = album.media$group.media$thumbnail[0].url;
-      var url = _.find(album.link, function(link) { return link.rel === 'alternate'; }).href;
-
-      return {title: title, thumbnail: thumbnail, url: url};
-    });
-
-    var template = _.template($('#albums-template').html());
-    var albumHtml = template({albums: albumData});
-    $('#albums').html(albumHtml);
-  };
-
-  if (window.albums) {
-    showAlbums(window.albums);
-  } else {
-    window.storeAlbums = showAlbums;
-  }
 })(window, $);
