abb9791a37f3aab2017dda55b03f106d24284b79
Yevgeniy Brikman <brikis98@users.noreply.github.com>
2022-04-07 11:27:16 +0100

    Add 3rd edition early release

--- jekyll-serve.sh
+++ jekyll-serve.sh
@@ -2,10 +2,4 @@
 
 set -e
 
-LIMIT_POSTS="${LIMIT_POSTS:-5}"
-
-if [[ "$LIMIT_POSTS" -gt 0 ]]; then
-  echo -e "\033[1;33m[WARN] Telling jekyll to only generate the last $LIMIT_POSTS posts. To change this limit, set the LIMIT_POSTS environment variable.\033[0m"
-fi
-
-bundle exec jekyll serve --drafts --incremental --limit_posts "$LIMIT_POSTS" --host 0.0.0.0
+exec bundle exec jekyll serve --drafts --incremental --host 0.0.0.0
