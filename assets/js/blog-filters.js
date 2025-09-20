8b575a063fb16697fed389eb08cfd41872952663
Yevgeniy Brikman <brikis98@users.noreply.github.com>
2025-09-20 15:41:17 +0100

    Enable routing so search & filters are saved/loaded to/from URL

--- assets/js/blog-filters.js
+++ assets/js/blog-filters.js
@@ -84,11 +84,15 @@ const updateFilterButton = (filterButton, selectedFilterValues) => {
   }
 };
 
+const updateFilterButtons = (uiState) => {
+  updateFilterButton(filterByTypeButton, uiState[algoliaSearchIndex]?.refinementList?.['Type']);
+  updateFilterButton(filterByTagButton, uiState[algoliaSearchIndex]?.refinementList?.['Tags']);
+  updateFilterButton(filterByRatingButton, uiState[algoliaSearchIndex]?.refinementList?.['Rating']);
+};
+
 const labelMiddleware = () => ({
   onStateChange({ uiState }) {
-    updateFilterButton(filterByTypeButton, uiState[algoliaSearchIndex]?.refinementList?.['Type']);
-    updateFilterButton(filterByTagButton, uiState[algoliaSearchIndex]?.refinementList?.['Tags']);
-    updateFilterButton(filterByRatingButton, uiState[algoliaSearchIndex]?.refinementList?.['Rating']);
+    updateFilterButtons(uiState);
   },
   subscribe() {},
   unsubscribe() {},
@@ -114,6 +118,7 @@ const search = instantsearch({
   indexName: algoliaSearchIndex,
   searchClient,
   future: { preserveSharedStateOnUnmount: true },
+  routing: true,
 
   // Search requests that take longer than this will show the "loading" indicators. Searches that are faster will
   // not show the loading indicators; if they are that fast, we really don't need to, as the user won't even notice
@@ -252,6 +257,8 @@ search.on('render', () => {
 
 search.start();
 
+updateFilterButtons(search.getUiState());
+
 const filterButtonClicked = (event) => {
   // If we haven't done any search queries, then the list of available facets are not loaded for any of the filter
   // menus, so perform search query to populate thoes facet values.
@@ -267,205 +274,3 @@ filterByTypeButton.addEventListener('click', filterButtonClicked);
 filterByTagButton.addEventListener('click', filterButtonClicked);
 filterByRatingButton.addEventListener('click', filterButtonClicked);
 
-// const noResults = document.getElementById('no-results');
-// const pagination = document.getElementById('blog-pagination');
-// const loadingSpinner = document.getElementById('loading-spinner');
-//
-// let allBlogPosts = null;
-// let originalBlogPosts = null;
-//
-// const showLoadingSpinner = () => {
-//   loadingSpinner.classList.remove('display-none');
-// };
-//
-// const hideLoadingSpinner = () => {
-//   loadingSpinner.classList.add('display-none');
-// };
-//
-// const convertBlogPostsToDomNodes = (parsedBlogPosts, originalBlogPosts) => {
-//   const sampleBlogPost = originalBlogPosts[0];
-//   return parsedBlogPosts.map(parsedBlogPost => {
-//     const clone = sampleBlogPost.cloneNode(true);
-//
-//     clone.dataset.id = parsedBlogPost.id;
-//     clone.querySelector('.post-title').innerText = parsedBlogPost.title;
-//     clone.querySelector('.post-date').dateTime = parsedBlogPost.date;
-//     clone.querySelector('.post-excerpt').innerText = parsedBlogPost.excerpt;
-//
-//     // TODO: need links for tags
-//     const postTags = Array.from(clone.querySelectorAll('.post-tag'));
-//     const sampleTag = postTags[0];
-//     const cloneTags = parsedBlogPost.tags.map(tag => {
-//       const cloneTag = sampleTag.cloneNode(true);
-//       cloneTag.innerText = tag;
-//       return cloneTag;
-//     })
-//     clone.querySelector('.post-tags').replaceChildren(...cloneTags);
-//
-//     // TODO: need image URLs
-//     // TODO: need comments URLs
-//     // TODO: need to fill in post URL
-//
-//     return clone;
-//   });
-// };
-//
-// const showDefaultBlogPosts = () => {
-//   pagination.classList.remove('display-none');
-//
-//   blogPostsContainer.replaceChildren(...originalBlogPosts);
-// };
-//
-// const updatePostCount = () => {
-//   const visibleBlogPosts = Array.from(blogPostsContainer.children).reduce((visibleCount, blogPost) => {
-//     return visibleCount + (blogPost.checkVisibility() ? 1 : 0);
-//   }, 0)
-//
-//   blogPostCount.innerText = visibleBlogPosts.toString();
-//
-//   if (visibleBlogPosts === 0) {
-//     noResults.classList.remove('display-none');
-//   } else {
-//     noResults.classList.add('display-none');
-//   }
-// };
-//
-// const encodeForHash = (values) => {
-//   return encodeURIComponent(values.join(';'));
-// };
-//
-// const updateUrlHash = () => {
-//   const searchText = searchBlog.value.trim();
-//   const selectedTypes = filterByTypeMultiSelect.getSelects();
-//   const selectedTags = filterByTagMultiSelect.getSelects();
-//   const selectedRatings = filterByRatingMultiSelect.getSelects();
-//   const sortType = sortMultiSelect.getSelects()[0];
-//
-//   const hashParts = [];
-//
-//   if (searchText.length > 0) {
-//     hashParts.push(`search=${encodeForHash([searchText])}`);
-//   }
-//
-//   if (selectedTypes.length > 0) {
-//     hashParts.push(`types=${encodeForHash(selectedTypes)}`);
-//   }
-//
-//   if (selectedTags.length > 0) {
-//     hashParts.push(`tags=${encodeForHash(selectedTags)}`);
-//   }
-//
-//   if (selectedRatings.length > 0) {
-//     hashParts.push(`ratings=${encodeForHash(selectedRatings)}`);
-//   }
-//
-//   if (sortType.length > 0) {
-//     hashParts.push(`sort=${encodeForHash([sortType])}`);
-//   }
-//
-//   window.location.hash = hashParts.join('&');
-// };
-//
-// const enableFiltersAndSortFromUrlHash = async () => {
-//   const parsedHash = new URLSearchParams(window.location.hash.substring(1));
-//   let filterChanged = false;
-//
-//   if (parsedHash.has('search')) {
-//     searchBlog.value = parsedHash.get('search');
-//     filterChanged = true;
-//   }
-//
-//   if (parsedHash.has('types')) {
-//     filterByTypeMultiSelect.setSelects(parsedHash.get('types').split(';'));
-//     filterChanged = true;
-//   }
-//
-//   if (parsedHash.has('tags')) {
-//     filterByTagMultiSelect.setSelects(parsedHash.get('tags').split(';'));
-//     filterChanged = true;
-//   }
-//
-//   if (parsedHash.has('ratings')) {
-//     filterByRatingMultiSelect.setSelects(parsedHash.get('ratings').split(';'));
-//     filterChanged = true;
-//   }
-//
-//   if (filterChanged) {
-//     await onFilterChange();
-//   }
-//
-//   if (parsedHash.has('sort')) {
-//     sortMultiSelect.setSelects([parsedHash.get('sort')]);
-//     onSortChange();
-//   }
-//
-//   window.addEventListener("hashchange", enableFiltersAndSortFromUrlHash);
-// };
-//
-// const searchBlogForText = async (searchText) => {
-//   if (searchText.length === 0) {
-//     return [];
-//   }
-//
-//   const index = await loadSearchIndex();
-//   const results = index.search({
-//     query: searchText,
-//     enrich: true,
-//     highlight: {
-//       template: '<mark>$1</mark>',
-//       boundary: {
-//         total: 200,
-//         before: 100,
-//         after: 100
-//       },
-//       ellipsis: ' [...] ',
-//       clip: false
-//     }
-//   });
-//
-//   // FlexSearch returns results grouped by the field that was matched (e.g., title or content). We want the list of
-//   // matching blog posts, deduped, each with its field, highlight, and doc information.
-//   const blogPostIdsToData = {};
-//
-//   results.forEach(resultForField => {
-//     resultForField.result.forEach(result => {
-//       const blogPost = blogPostIdsToData[result.id] || {id: result.id, doc: result.doc, matches: []};
-//       blogPost.matches.push({field: resultForField.field, highlight: result.highlight});
-//       blogPostIdsToData[result.id] = blogPost;
-//     });
-//   });
-//
-//   return Object.values(blogPostIdsToData);
-// };
-//
-// const onFilterChange = async () => {
-//   showLoadingSpinner();
-//
-//   const searchText = searchBlog.value.trim();
-//   const selectedTypes = filterByTypeMultiSelect.getSelects();
-//   const selectedTags = filterByTagMultiSelect.getSelects();
-//   const selectedRatings = filterByRatingMultiSelect.getSelects();
-//
-//   const selectedFilters = [].concat(selectedTypes, selectedTags, selectedRatings);
-//   if (searchText.length > 0 || selectedFilters.length > 0) {
-//     // Fetch both in parallel
-//     const blogPostsPromise = getAllBlogPosts();
-//     const blogPostsMatchingSearchPromise = searchBlogForText(searchText);
-//
-//     const blogPosts = await blogPostsPromise;
-//     const blogPostsMatchingSearch = await blogPostsMatchingSearchPromise;
-//
-//     filterBlogPosts(blogPosts, blogPostsMatchingSearch, searchText, selectedTypes, selectedTags, selectedRatings);
-//   } else {
-//     showDefaultBlogPosts();
-//   }
-//
-//   updatePostCount();
-//   updateUrlHash();
-//
-//   hideLoadingSpinner();
-// };
-//
-// (async () => {
-//   await enableFiltersAndSortFromUrlHash();
-// })();
